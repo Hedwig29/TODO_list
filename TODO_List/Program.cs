4efb00198736d476795070bed9c77a9976e63a5a
Hedwig29 <156532418+Hedwig29@users.noreply.github.com>
2024-02-12 17:15:13 -0600

    1st version

--- TODO_List/Program.cs
+++ TODO_List/Program.cs
@@ -2,6 +2,7 @@
 
 class Program
 {
+   
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
@@ -12,10 +13,47 @@ class Program
         // После выполнения действия программа должна снова выводить меню и ждать дальнейших действий пользователя.
         // Все действия должны выполняться до тех пор, пока пользователь не выберет пункт "Выход".
         
-        
+        List<string> toDoList = new List<string>();
+
+       
         while (true)
         {
+            if (toDoList.Count > 0) { }
+            
             Console.WriteLine("Список дел:");
+            Console.WriteLine("Выберите действия:");
+            Console.WriteLine("1. Добавить задачу");
+            Console.WriteLine("2. Удалить задачу");
+            Console.WriteLine("3. Выход");
+            string choice = (Console.ReadLine());
+            switch (choice) 
+            {
+                case "1":
+                Console.WriteLine("Введите задачу: ");
+                string task = Console.ReadLine();
+                toDoList.Add(task);
+                
+                Console.WriteLine("Задача добавлена успешно");
+                break;
+                case "2":
+                Console.WriteLine("Введите номер задачи для удаления: ");
+                string task = Console.ReadLine();
+                toDoList.Remove(task);
+                Console.WriteLine("Задача удалена успешно");
+                break;
+                case "3":
+                Console.Clear();
+                break;
+                default:
+                Console.WriteLine("Неизвестная команда,повторите попытку");
+                Console.WriteLine("Для выхода нажмите любую клавишу...");
+                Console.ReadKey();
+                Console.Clear();
+                break;
+
+
+
+            }
             // Выводим список задач
             
             // Выводим меню
